960e19009f8bffa7f188d17301e65224e0398074
quentincabanes-svg <quentin.cabanes@gmail.com>
2025-08-31 16:10:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/neuroreact/", // ex: "/neuroreact/"
+  base: "/NeuroReact/", // ex: "/neuroreact/"
 });
