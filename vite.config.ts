b18a5ee3202e2ca976b254dd309161d07400760f
quentincabanes-svg <quentin.cabanes@gmail.com>
2025-08-31 15:59:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ mets exactement le nom de ton dépôt entre les slashes :
-  base: "/neuroreact/",
+  base: "/NOM_DU_DEPOT/", // ex: "/neuroreact/"
 });
